9eaa1d8507189bff909fc76336f6e6c0b805d87e
Aluno <Aluno@SNCTM202LABF09>
2024-01-31 21:05:26 -0300

    Adicionar arquivos de projeto.

---
+++ app_caracoroa/app_caracoroa/PageJogo.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCaraCoroa
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class PageJogo : ContentPage
+    {
+        public PageJogo()
+        {
+            InitializeComponent();
+            Random r = new Random();
+            int n = r.Next(1, 6);
+            switch(n)
+            {
+                case 1:
+                    imgDado.Source = "dado1.png";
+                    break;
+                case 2:
+                    imgDado.Source = "dado2.png";
+                    break;
+                case 3:
+                    imgDado.Source = "dado3.png";
+                    break;
+                case 4:
+                    imgDado.Source = "dado4.png";
+                    break;
+                case 5:
+                    imgDado.Source = "dado5.png";
+                    break;
+                case 6:
+                    imgDado.Source = "dado6.png";
+                    break;
+
+
+            }
+        }
+
+        private async void btVoltar_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
